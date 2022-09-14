979dbd0240eda03b8f245bbc00f90852563f7727
lm-glovory <hossin@glovary.com>
2022-09-14 15:25:16 +0600

    bug fixed

--- controllers/DoctorController.js
+++ controllers/DoctorController.js
@@ -1,22 +1,28 @@
 const Doctor = require('../models/Doctor');
-exports.getAllDoctor =async (req,res,next)=>{
-    try{
+exports.getAllDoctor = async (req, res, next) => {
+    try {
         const doctor = await Doctor.find();
-        res.json({doctor});
-    }catch(e){
+        res.json({
+            doctor
+        });
+    } catch (e) {
         next(e);
     }
 }
 
-exports.addDoctor = async (req, res,next) => {
-    try{
-        const doctor = await Doctor.create({
+exports.addDoctor = async (req, res, next) => {
+    console.log(req.body);
+    try {
+        await Doctor.create({
             name: req.body.name,
             email: req.body.email,
             specialty: req.body.specialty,
         });
-        res.json({doctor:await Doctor.find()});
-    }catch(e){
+        res.json({
+            doctor: await Doctor.find(),
+            status:200
+        });
+    } catch (e) {
         next(e);
     }
 }
